3b5489615170a3bea3faa1403cc8ae8d8ec4e60c
julietvt <freshitpt@gmail.com>
2021-02-27 10:23:53 +0200

    clear file

--- assets/view_model/index.js
+++ assets/view_model/index.js
@@ -1,8 +1,1 @@
 'use strict';
-//import { sum, sub  } from '../model/index';
-import def, * as calcul from '../model/index.js';
-console.log(calcul.sum(2, 2));
-console.log(calcul.sub(2, 2));
-console.log(calcul.mult(2, 2));
-console.log(calcul.div(2, 2));
-console.log(calcul);
