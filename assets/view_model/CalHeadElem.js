29fcf161ad89e5dae3a1f3ec42d2e7a232727c14
julietvt <freshitpt@gmail.com>
2021-02-27 12:17:49 +0200

    add view model CalHeadElem

---
+++ assets/view_model/CalHeadElem.js
@@ -0,0 +1,25 @@
+'use strict';
+
+export default function (month) {
+  const tableHeadElem = document.createElement('thead');
+  const tableRow = document.createElement('tr');
+  const tableH = document.createElement('th');
+  tableH.setAttribute('colspan', 7);
+  tableH.classList.add('calMonthName');
+  tableH.innerText = `${month.date.toLocaleString('ru-Ru', {
+    month: 'long',
+  })} ${month.date.getFullYear()}`;
+  tableRow.appendChild(tableH);
+  tableHeadElem.appendChild(tableRow);
+
+  const tableR = document.createElement('tr');
+  tableR.classList.add('calWeekDays');
+  month.weeks[0].days.forEach((day) => {
+    const tableHeader = document.createElement('th');
+    tableHeader.innerText = `${day.date.toLocaleString('ru-RU', {
+      weekday: 'short',
+    })}`;
+    tableR.appendChild(tableHeader);
+  });
+  tableHeadElem.appendChild(tableR);
+}
